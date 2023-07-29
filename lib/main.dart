0a7925da45630c09de02c21ff968e0907648a5ec
Sanghoon Lee <nasi3611@naver.com>
2023-07-29 21:41:00 +0900

    앱바 완성

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:cupertino_icons/cupertino_icons.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const InstaCloneApp());
@@ -9,8 +12,45 @@ class InstaCloneApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: const ColorScheme.light(
+            primary: Colors.white, secondary: Colors.black),
+        useMaterial3: true,
+      ),
+      home: const InstaCloneHome(),
+    );
   }
 }
 
+class InstaCloneHome extends StatelessWidget {
+  const InstaCloneHome({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Instagram',
+          style: GoogleFonts.lobsterTwo(color: Colors.black, fontSize: 32),
+        ),
+        centerTitle: false,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.favorite_outline, size: 32),
+            onPressed: () {
+              print('Tab Favorite');
+            },
+          ),
+          IconButton(
+            icon: const Icon(CupertinoIcons.paperplane, size: 32),
+            onPressed: () {
+              print('Tab PaperPlane');
+            },
+          ),
+        ],
+      ),
+      body: InstaBody(),
+    );
+  }
+}
